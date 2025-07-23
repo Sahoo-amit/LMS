a9590440bdc84dc5ffd29f1bca07eed94d336f49
Amit Sahoo <amitsahooamit456@gmail.com>
2025-07-23 23:11:59 +0530

    error resolved

---
+++ server/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./config/db.js";
+import authRouter from "./routes/auth.route.js";
+import courseRouter from "./routes/course.route.js";
+import mediaRouter from "./routes/media.route.js";
+import purchaseRouter from "./routes/purchase.route.js";
+import progressRouter from "./routes/progress.route.js";
+import contactRouter from "./routes/contact.route.js";
+import cors from "cors";
+
+config();
+
+const app = express();
+
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOption));
+app.use(express.json());
+
+app.use("/api/media", mediaRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/purchase", purchaseRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/progress", progressRouter);
+app.use("/api/contact", contactRouter);
+
+connectDB();
+
+export default app
--- server/index.js
+++ server/index.js
@@ -1,34 +1,7 @@
-import express from 'express'
-import { config } from "dotenv"
-import { connectDB } from './config/db.js';
-import authRouter from './routes/auth.route.js'
-import courseRouter from './routes/course.route.js'
-import mediaRouter from './routes/media.route.js'
-import purchaseRouter from './routes/purchase.route.js'
-import progressRouter from './routes/progress.route.js'
-import contactRouter from './routes/contact.route.js'
-import cors from 'cors'
+import app from "./app";
+import {createServer} from 'http'
 
-config();
-
-const app = express()
-
-const corsOption = {
-  origin: process.env.FRONTEND_URL,
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  credentials: true,
-};
-app.use(cors(corsOption))
-app.use(express.json())
-
-app.use('/api/media', mediaRouter)
-app.use('/api/auth', authRouter)
-app.use("/api/purchase", purchaseRouter)
-app.use('/api/courses', courseRouter)
-app.use('/api/progress', progressRouter)
-app.use("/api/contact", contactRouter)
-const PORT = process.env.PORT || 4000
-connectDB()
-app.listen(PORT, ()=>{
-    console.log(`Server is listening on ${PORT}`)
-})
+export default function handler(req, res) {
+  const server = createServer(app);
+  return server.emit("request", req, res);
+}
