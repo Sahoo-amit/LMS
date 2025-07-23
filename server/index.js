8c84d07adb7c7d1f30c977557d7356b214fce86d
Amit Sahoo <amitsahooamit456@gmail.com>
2025-07-23 23:17:10 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
-import app from "./app";
-import {createServer} from 'http'
+import express from 'express'
+import { config } from "dotenv"
+import { connectDB } from './config/db.js';
+import authRouter from './routes/auth.route.js'
+import courseRouter from './routes/course.route.js'
+import mediaRouter from './routes/media.route.js'
+import purchaseRouter from './routes/purchase.route.js'
+import progressRouter from './routes/progress.route.js'
+import contactRouter from './routes/contact.route.js'
+import cors from 'cors'
 
-export default function handler(req, res) {
-  const server = createServer(app);
-  return server.emit("request", req, res);
-}
+config();
+
+const app = express()
+
+app.use(express.json())
+const corsOption = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOption))
+
+app.use('/api/media', mediaRouter)
+app.use('/api/auth', authRouter)
+app.use("/api/purchase", purchaseRouter)
+app.use('/api/courses', courseRouter)
+app.use('/api/progress', progressRouter)
+app.use("/api/contact", contactRouter)
+const PORT = process.env.PORT || 4000
+connectDB()
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
+})
