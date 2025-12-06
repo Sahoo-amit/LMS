47a8e034da490b26af559a5f76bbf5d9150ce084
Amit Sahoo <amitsahooamit456@gmail.com>
2025-12-06 13:25:16 +0530

    .

--- server/index.js
+++ server/index.js
@@ -13,26 +13,36 @@ import { stripeWebhook } from "./controllers/payment.controller.js";
 const FRONTEND_URL = "https://lms-ntj1.onrender.com";
 
 config();
-
 const app = express();
 
-const corsOption = {
-  origin: FRONTEND_URL,
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  credentials: true,
-};
-app.use(cors(corsOption));
-app.post("/webhook", express.raw({ type: "application/json" }), stripeWebhook)
+// ⭐ MUST COME BEFORE express.json()
+app.post(
+  "/api/purchase/webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhook
+);
 
+// CORS
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
+// ⭐ JSON parser AFTER webhook
 app.use(express.json());
 
+// ROUTES
 app.use("/api/media", mediaRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/purchase", purchaseRouter);
 app.use("/api/courses", courseRouter);
 app.use("/api/progress", progressRouter);
 app.use("/api/contact", contactRouter);
+
+// START SERVER
 const PORT = process.env.PORT || 4000;
 connectDB();
 app.listen(PORT, () => {
