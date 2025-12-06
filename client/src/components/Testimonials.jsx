ccc5ae9dd32456c9d28cf66665ec598345327a56
Amit Sahoo <amitsahooamit456@gmail.com>
2025-12-06 11:13:50 +0530

    .

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -96,13 +96,10 @@ const Testimonials = () => {
                 alt="course"
                 className="w-full h-40 object-cover rounded-md mb-3"
               />
-              {isAuthenticated ? (
-                <Link to={`/courses/${_id}`}>
-                  <h3 className="text-lg sm:text-xl font-semibold">{title}</h3>
-                </Link>
-              ) : (
-                <Link to={"/signin"} />
-              )}
+              <Link to={isAuthenticated ? `/courses/${_id}` : "/signin"}>
+                <h3 className="text-lg sm:text-xl font-semibold">{title}</h3>
+              </Link>
+
               <p className="mt-2 font-semibold">Price: Rs.{price}</p>
               <p className="text-yellow-500 font-semibold"></p>
               <p className="my-1 font-semibold text-sm">
